33d64348d153f9436ded3edbd98f7dd2eba5f180
Bhakti Jain <69834356+jain-bhakti@users.noreply.github.com>
2023-05-20 17:47:47 +0530

    Create 399_Evaluate_division.py

---
+++ Leetcode/399_Evaluate_division.py
@@ -0,0 +1,26 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        graph =  defaultdict(dict)
+        N = len(equations)
+        for i in range(N):
+            graph[equations[i][0]][equations[i][1]] = values[i]
+            graph[equations[i][1]][equations[i][0]] = 1/values[i]
+
+        def dfs(x,y,visited):
+            if x not in graph or y not in graph: return -1
+
+            if y in graph[x]:
+                return graph[x][y]
+            for i in graph[x]:
+                if i not in visited:
+                    visited.add(i)
+                    temp = dfs(i,y,visited)
+                    if temp == -1:
+                        continue
+                    else:
+                        return temp*graph[x][i]
+            return -1
+        output = []
+        for p,q in queries:
+            output.append(dfs(p,q,set()))
+        return output
